248dd1aa0384748a4820bb8b847938bba8a5d5f1
hugesilver <xor3160@naver.com>
2022-07-14 13:42:52 +0900

    Refactored announcement codes

--- receive_announcement.py
+++ receive_announcement.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
+from bs4 import BeautifulSoup
 
 option = Options()
 option.add_argument('--headless')
@@ -25,3 +26,55 @@ driver.quit() # 웹드라이버 종료
 f990 = open("./data/html_990.txt", 'w')
 f990.write(html_990)
 f990.close()
+
+f900 = open("./data/html_900.txt", 'r')
+html = f900.read()
+f900.close()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+m_900 = open("./data/l_bachelor.json", 'w')
+
+m_900.write('{"version": "2.0","template": {"outputs": [{"listCard": {"header": {"title": "대림대학교 학사공지"},"items": [')
+
+for i in range(5):
+    m_900.write('{')
+    m_900.write('"title": "{}",'.format(soup.select_one('#tbody > li:nth-child(' + str(i + 1) + ') > .txtL > a').get_text()))
+    m_900.write('"description": "{}",'.format(soup.select_one('#tbody > li:nth-child(' + str(i + 1) + ') > .txtInfo > .date').get_text()))
+    m_900.write('"link": {')
+    m_900.write('"web": "{}"'.format("https://www.daelim.ac.kr" + ((((str(soup.select_one('#tbody > li:nth-child(' + str(i + 1) + ') > .txtL > a')).split())[6])[6:])[:-1]).replace("amp;", "")))
+    m_900.write('}')
+    if(i != 4):
+        m_900.write('},')
+    else:
+        m_900.write('}')
+
+m_900.write('],')
+m_900.write('"buttons": [{"label": "학사공지 전체보기", "action": "webLink", "webLinkUrl": "https://www.daelim.ac.kr/cms/FrCon/index.do?MENU_ID=900#page1"}]}}]}}')
+m_900.close()
+
+f990 = open("./data/html_990.txt", 'r')
+html = f990.read()
+f990.close()
+
+soup = BeautifulSoup(html, 'html.parser')
+
+m_990 = open("./data/l_scholar.json", 'w')
+
+m_990.write('{"version": "2.0","template": {"outputs": [{"listCard": {"header": {"title": "대림대학교 장학 및 대출공지"},"items": [')
+
+for i in range(5):
+    m_990.write('{')
+    m_990.write('"title": "{}",'.format(soup.select_one('#tbody > li:nth-child(' + str(i + 1) + ') > .txtL > a').get_text()))
+    m_990.write('"description": "{}",'.format(soup.select_one('#tbody > li:nth-child(' + str(i + 1) + ') > .txtInfo > .date').get_text()))
+    m_990.write('"link": {')
+    m_990.write('"web": "{}"'.format("https://www.daelim.ac.kr" + ((((str(soup.select_one('#tbody > li:nth-child(' + str(i + 1) + ') > .txtL > a')).split())[6])[6:])[:-1]).replace("amp;", "")))
+    m_990.write('}')
+    if(i != 4):
+        m_990.write('},')
+    else:
+        m_990.write('}')
+
+m_990.write('],')
+m_990.write('"buttons": [{"label": "장학 및 대출공지 전체보기", "action": "webLink", "webLinkUrl": "https://www.daelim.ac.kr/cms/FrCon/index.do?MENU_ID=990#page1"}]}}]}}')
+m_990.close()

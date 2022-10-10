aa4105caf56b8fe021603acf63b1028af402319a
hugesilver <xor3160@naver.com>
2022-10-10 15:04:22 +0900

    Untied spaghetti codes

--- schoolbus.py
+++ schoolbus.py
@@ -31,28 +31,32 @@ m_460_anyang_to_school.write("안양역에서 학교로 이동하는 셔틀버�
 m_460_anyang_to_school.write("\\n")
 
 for anyang_to_school in anyang_tr:
-    if ((anyang_to_school.select_one('td:nth-child(1)').get_text()) == " ") or ((anyang_to_school.select_one('td:nth-child(1)').get_text()) == "") or ((anyang_to_school.select_one('td:nth-child(1)').get_text()) == "휴게시간"):
+    if ((anyang_to_school.select_one('td:nth-child(1)').get_text()) in ["휴게시간", "", " ", "&nbsp;", " "]):
         pass
-    elif (((anyang_to_school.select_one('td:nth-child(1)').get_text())[1].isdigit() == False) and (((anyang_to_school.select_one('td:nth-child(2)').get_text()) == "") or (anyang_to_school.select_one('td:nth-child(2)').get_text()) == " ")):
+    elif (((anyang_to_school.select_one('td:nth-child(1)').get_text())[1].isdigit() == False) and ((anyang_to_school.select_one('td:nth-child(2)').get_text() in ["", " ", "&nbsp;", " "]))):
         pass
     elif (anyang_to_school.select_one('td:nth-child(1)').get_text())[1].isdigit() == True:
-        m_460_anyang_to_school.write("- ")
-        m_460_anyang_to_school.write(anyang_to_school.select_one('td:nth-child(1)').get_text() + " ")
-        if (anyang_to_school.select_one('td:nth-child(3)').get_text() == "해당시간") or (anyang_to_school.select_one('td:nth-child(3)').get_text() == "") or (anyang_to_school.select_one('td:nth-child(3)').get_text() == " "):
-            m_460_anyang_to_school.write("\\n")
+        if (anyang_to_school.select_one('td:nth-child(1)').get_text() in ["", " ", "&nbsp;", " "]):
+            pass
         else:
-            m_460_anyang_to_school.write("(배차간격: {})\\n".format(anyang_to_school.select_one('td:nth-child(3)').get_text()))
+            m_460_anyang_to_school.write("- ")
+            m_460_anyang_to_school.write(anyang_to_school.select_one('td:nth-child(1)').get_text() + " ")
+            if (anyang_to_school.select_one('td:nth-child(3)').get_text() in ["해당시간", "", " ", "&nbsp;", " "]):
+                m_460_anyang_to_school.write("\\n")
+            else:
+                m_460_anyang_to_school.write("(배차간격: {})\\n".format(anyang_to_school.select_one('td:nth-child(3)').get_text()))
     elif (anyang_to_school.select_one('td:nth-child(1)').get_text())[1].isdigit() == False:
-        if (anyang_to_school.select_one('td:nth-child(2)').get_text()) == "":
+        if (anyang_to_school.select_one('td:nth-child(2)').get_text() in ["", " ", "&nbsp;", " "]):
             pass
         else:
             m_460_anyang_to_school.write("- ")
             m_460_anyang_to_school.write(anyang_to_school.select_one('td:nth-child(2)').get_text() + " ")
-            if (anyang_to_school.select_one('td:nth-child(4)').get_text() == "해당시간") or (anyang_to_school.select_one('td:nth-child(4)').get_text() == "") or (anyang_to_school.select_one('td:nth-child(3)').get_text() == " "):
+            if (anyang_to_school.select_one('td:nth-child(4)').get_text() in ["해당시간", "", " ", "&nbsp;", " "]):
                 m_460_anyang_to_school.write("\\n")
             else:
                 m_460_anyang_to_school.write("(배차간격: {})\\n".format(anyang_to_school.select_one('td:nth-child(4)').get_text()))
 
+m_460_anyang_to_school.write("\\n※ 교통 혼잡 및 신호대기로 인해 운행시간이 변동될 수 있습니다.")
 m_460_anyang_to_school.write('"}}],"quickReplies":[{"action": "block", "messageText": "🚌 전체 셔틀버스 배차시간", "label": "🚌 전체 셔틀버스 배차시간", "blockId": "633e69ddca1fd2777db9a2a8"},{"action": "block", "messageText": "☕️ 커피 후원하기", "label": "☕️ 커피 후원하기", "blockId": "633e658052a78f5479d6acea"}]}}')
 m_460_anyang_to_school.close()
 
@@ -68,21 +72,28 @@ m_460_school_to_anyang.write("\\n")
 for school_to_anyang in anyang_tr:
     if ((school_to_anyang.select_one('td:nth-child(1)').get_text()) == "휴게시간"):
         pass
-    elif (school_to_anyang.select_one('td:nth-child(1)').get_text() == " ") or (school_to_anyang.select_one('td:nth-child(1)').get_text() == "") or ((school_to_anyang.select_one('td:nth-child(1)').get_text())[1].isdigit() == True):
-        m_460_school_to_anyang.write("- ")
-        m_460_school_to_anyang.write(school_to_anyang.select_one('td:nth-child(2)').get_text() + " ")
-        if (school_to_anyang.select_one('td:nth-child(3)').get_text() == "해당시간") or (school_to_anyang.select_one('td:nth-child(3)').get_text() == "") or (school_to_anyang.select_one('td:nth-child(3)').get_text() == " "):
-            m_460_school_to_anyang.write("\\n")
+    elif (school_to_anyang.select_one('td:nth-child(1)').get_text() in ["", " ", "&nbsp;", " "]) or ((school_to_anyang.select_one('td:nth-child(1)').get_text())[1].isdigit() == True):
+        if(school_to_anyang.select_one('td:nth-child(2)').get_text() in ["", " ", "&nbsp;", " "]):
+            pass
         else:
-            m_460_school_to_anyang.write("(배차간격: {})\\n".format(school_to_anyang.select_one('td:nth-child(3)').get_text()))
+            m_460_school_to_anyang.write("- ")
+            m_460_school_to_anyang.write(school_to_anyang.select_one('td:nth-child(2)').get_text() + " ")
+            if (school_to_anyang.select_one('td:nth-child(3)').get_text() in ["해당시간", "", " ", "&nbsp;", " "]):
+                m_460_school_to_anyang.write("\\n")
+            else:
+                m_460_school_to_anyang.write("(배차간격: {})\\n".format(school_to_anyang.select_one('td:nth-child(3)').get_text()))
     elif (school_to_anyang.select_one('td:nth-child(1)').get_text())[1].isdigit() == False:
-        m_460_school_to_anyang.write("- ")
-        m_460_school_to_anyang.write(school_to_anyang.select_one('td:nth-child(3)').get_text() + " ")
-        if (school_to_anyang.select_one('td:nth-child(4)').get_text() == "해당시간") or (school_to_anyang.select_one('td:nth-child(4)').get_text() == "") or (school_to_anyang.select_one('td:nth-child(3)').get_text() == " "):
-            m_460_school_to_anyang.write("\\n")
+        if(school_to_anyang.select_one('td:nth-child(3)').get_text() in ["", " ", "&nbsp;", " "]):
+            pass
         else:
-            m_460_school_to_anyang.write("(배차간격: {})\\n".format(school_to_anyang.select_one('td:nth-child(4)').get_text()))
+            m_460_school_to_anyang.write("- ")
+            m_460_school_to_anyang.write(school_to_anyang.select_one('td:nth-child(3)').get_text() + " ")
+            if (school_to_anyang.select_one('td:nth-child(4)').get_text() in ["해당시간", "", " ", "&nbsp;", " "]):
+                m_460_school_to_anyang.write("\\n")
+            else:
+                m_460_school_to_anyang.write("(배차간격: {})\\n".format(school_to_anyang.select_one('td:nth-child(4)').get_text()))
 
+m_460_school_to_anyang.write("\\n※ 교통 혼잡 및 신호대기로 인해 운행시간이 변동될 수 있습니다.")
 m_460_school_to_anyang.write('"}}],"quickReplies":[{"action": "block", "messageText": "🚌 전체 셔틀버스 배차시간", "label": "🚌 전체 셔틀버스 배차시간", "blockId": "633e69ddca1fd2777db9a2a8"},{"action": "block", "messageText": "☕️ 커피 후원하기", "label": "☕️ 커피 후원하기", "blockId": "633e658052a78f5479d6acea"}]}}')
 m_460_school_to_anyang.close()
 
@@ -101,28 +112,32 @@ m_460_beomgye_to_school.write("범계역에서 학교로 이동하는 셔틀버�
 m_460_beomgye_to_school.write("\\n")
 
 for beomgye_to_school in beomgye_tr:
-    if ((beomgye_to_school.select_one('td:nth-child(1)').get_text()) == " ") or ((beomgye_to_school.select_one('td:nth-child(1)').get_text()) == "") or ((beomgye_to_school.select_one('td:nth-child(1)').get_text()) == "휴게시간"):
+    if ((beomgye_to_school.select_one('td:nth-child(1)').get_text()) in ["휴게시간", "", " ", "&nbsp;", " "]):
         pass
-    elif (((beomgye_to_school.select_one('td:nth-child(1)').get_text())[1].isdigit() == False) and (((beomgye_to_school.select_one('td:nth-child(2)').get_text()) == "") or (beomgye_to_school.select_one('td:nth-child(2)').get_text()) == " ")):
+    elif (((beomgye_to_school.select_one('td:nth-child(1)').get_text())[1].isdigit() == False) and (((beomgye_to_school.select_one('td:nth-child(2)').get_text()) in ["", " ", "&nbsp;", " "]))):
         pass
     elif (beomgye_to_school.select_one('td:nth-child(1)').get_text())[1].isdigit() == True:
-        m_460_beomgye_to_school.write("- ")
-        m_460_beomgye_to_school.write(beomgye_to_school.select_one('td:nth-child(1)').get_text() + " ")
-        if (beomgye_to_school.select_one('td:nth-child(3)').get_text() == "해당시간") or (beomgye_to_school.select_one('td:nth-child(3)').get_text() == "") or (beomgye_to_school.select_one('td:nth-child(3)').get_text() == " "):
-            m_460_beomgye_to_school.write("\\n")
+        if(beomgye_to_school.select_one('td:nth-child(1)').get_text() in ["", " ", "&nbsp;", " "]):
+            pass
         else:
-            m_460_beomgye_to_school.write("(배차간격: {})\\n".format(beomgye_to_school.select_one('td:nth-child(3)').get_text()))
+            m_460_beomgye_to_school.write("- ")
+            m_460_beomgye_to_school.write(beomgye_to_school.select_one('td:nth-child(1)').get_text() + " ")
+            if (beomgye_to_school.select_one('td:nth-child(3)').get_text() in ["해당시간", "", " ", "&nbsp;", " "]):
+                m_460_beomgye_to_school.write("\\n")
+            else:
+                m_460_beomgye_to_school.write("(배차간격: {})\\n".format(beomgye_to_school.select_one('td:nth-child(3)').get_text()))
     elif (beomgye_to_school.select_one('td:nth-child(1)').get_text())[1].isdigit() == False:
-        if (beomgye_to_school.select_one('td:nth-child(2)').get_text()) == "":
+        if (beomgye_to_school.select_one('td:nth-child(2)').get_text() in ["", " ", "&nbsp;", " "]):
             pass
         else:
             m_460_beomgye_to_school.write("- ")
             m_460_beomgye_to_school.write(beomgye_to_school.select_one('td:nth-child(2)').get_text() + " ")
-            if (beomgye_to_school.select_one('td:nth-child(4)').get_text() == "해당시간") or (beomgye_to_school.select_one('td:nth-child(4)').get_text() == "") or (beomgye_to_school.select_one('td:nth-child(3)').get_text() == " "):
+            if (beomgye_to_school.select_one('td:nth-child(4)').get_text() in ["해당시간", "", " ", "&nbsp;", " "]):
                 m_460_beomgye_to_school.write("\\n")
             else:
                 m_460_beomgye_to_school.write("(배차간격: {})\\n".format(beomgye_to_school.select_one('td:nth-child(4)').get_text()))
 
+m_460_beomgye_to_school.write("\\n※ 교통 혼잡 및 신호대기로 인해 운행시간이 변동될 수 있습니다.")
 m_460_beomgye_to_school.write('"}}],"quickReplies":[{"action": "block", "messageText": "🚌 전체 셔틀버스 배차시간", "label": "🚌 전체 셔틀버스 배차시간", "blockId": "633e69ddca1fd2777db9a2a8"},{"action": "block", "messageText": "☕️ 커피 후원하기", "label": "☕️ 커피 후원하기", "blockId": "633e658052a78f5479d6acea"}]}}')
 m_460_beomgye_to_school.close()
 
@@ -138,20 +153,27 @@ m_460_school_to_beomgye.write("\\n")
 for school_to_beomgye in beomgye_tr:
     if ((school_to_beomgye.select_one('td:nth-child(1)').get_text()) == "휴게시간"):
         pass
-    elif (school_to_beomgye.select_one('td:nth-child(1)').get_text() == " ") or  (school_to_beomgye.select_one('td:nth-child(1)').get_text() == "") or ((school_to_beomgye.select_one('td:nth-child(1)').get_text())[1].isdigit() == True):
-        m_460_school_to_beomgye.write("- ")
-        m_460_school_to_beomgye.write(school_to_beomgye.select_one('td:nth-child(2)').get_text() + " ")
-        if (school_to_beomgye.select_one('td:nth-child(3)').get_text() == "해당시간") or (school_to_beomgye.select_one('td:nth-child(3)').get_text() == "") or (school_to_beomgye.select_one('td:nth-child(3)').get_text() == " "):
-            m_460_school_to_beomgye.write("\\n")
+    elif (school_to_beomgye.select_one('td:nth-child(1)').get_text() in ["", " ", "&nbsp;", " "]) or ((school_to_beomgye.select_one('td:nth-child(1)').get_text())[1].isdigit() == True):
+        if(school_to_beomgye.select_one('td:nth-child(2)').get_text() in ["", " ", "&nbsp;", " "]):
+            pass
         else:
-            m_460_school_to_beomgye.write("(배차간격: {})\\n".format(school_to_beomgye.select_one('td:nth-child(3)').get_text()))
+            m_460_school_to_beomgye.write("- ")
+            m_460_school_to_beomgye.write(school_to_beomgye.select_one('td:nth-child(2)').get_text() + " ")
+            if (school_to_beomgye.select_one('td:nth-child(3)').get_text() in ["해당시간", "", " ", "&nbsp;", " "]):
+                m_460_school_to_beomgye.write("\\n")
+            else:
+                m_460_school_to_beomgye.write("(배차간격: {})\\n".format(school_to_beomgye.select_one('td:nth-child(3)').get_text()))
     elif (school_to_beomgye.select_one('td:nth-child(1)').get_text())[1].isdigit() == False:
-        m_460_school_to_beomgye.write("- ")
-        m_460_school_to_beomgye.write(school_to_beomgye.select_one('td:nth-child(3)').get_text() + " ")
-        if (school_to_beomgye.select_one('td:nth-child(4)').get_text() == "해당시간") or (school_to_beomgye.select_one('td:nth-child(4)').get_text() == "") or (school_to_beomgye.select_one('td:nth-child(3)').get_text() == " "):
-            m_460_school_to_beomgye.write("\\n")
+        if (school_to_beomgye.select_one('td:nth-child(3)').get_text() in ["", " ", "&nbsp;", " "]):
+            pass
         else:
-            m_460_school_to_beomgye.write("(배차간격: {})\\n".format(school_to_beomgye.select_one('td:nth-child(4)').get_text()))
+            m_460_school_to_beomgye.write("- ")
+            m_460_school_to_beomgye.write(school_to_beomgye.select_one('td:nth-child(3)').get_text() + " ")
+            if (school_to_beomgye.select_one('td:nth-child(4)').get_text() in ["해당시간", "", " ", "&nbsp;", " "]):
+                m_460_school_to_beomgye.write("\\n")
+            else:
+                m_460_school_to_beomgye.write("(배차간격: {})\\n".format(school_to_beomgye.select_one('td:nth-child(4)').get_text()))
 
+m_460_school_to_beomgye.write("\\n※ 교통 혼잡 및 신호대기로 인해 운행시간이 변동될 수 있습니다.")
 m_460_school_to_beomgye.write('"}}],"quickReplies":[{"action": "block", "messageText": "🚌 전체 셔틀버스 배차시간", "label": "🚌 전체 셔틀버스 배차시간", "blockId": "633e69ddca1fd2777db9a2a8"},{"action": "block", "messageText": "☕️ 커피 후원하기", "label": "☕️ 커피 후원하기", "blockId": "633e658052a78f5479d6acea"}]}}')
 m_460_school_to_beomgye.close()

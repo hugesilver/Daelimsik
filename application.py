5dbdb88cab7dcbb42e844bfbdad1c0e4e749fbf4
hugesilver <xor3160@naver.com>
2022-11-28 02:03:02 +0900

    Fixed address

--- application.py
+++ application.py
@@ -79,7 +79,7 @@ def profstaff_thu():
 
 @app.route('/profstaff/fri', methods=["POST"])
 def profstaff_fri():
-    with open('./data/m_profstaff_fri.json', 'r') as profstaff_fri:
+    with open('./out/profstaff/m_profstaff_fri.json', 'r') as profstaff_fri:
         m_profstaff_fri = json.load(profstaff_fri)
     return m_profstaff_fri
 
@@ -87,19 +87,19 @@ def profstaff_fri():
 
 @app.route('/bachelor', methods=["POST"])
 def bachelor():
-    with open('./data/l_bachelor.json', 'r') as bachelor:
+    with open('./out/announcement/l_bachelor.json', 'r') as bachelor:
         l_bachelor = json.load(bachelor)
     return l_bachelor
 
 @app.route('/scholarship', methods=["POST"])
 def scholarship():
-    with open('./data/l_scholarship.json', 'r') as scholarship:
+    with open('./out/announcement/l_scholarship.json', 'r') as scholarship:
         l_scholarship = json.load(scholarship)
     return l_scholarship
 
 @app.route('/administrative', methods=["POST"])
 def administrative():
-    with open('./data/l_administrative.json', 'r') as administrative:
+    with open('./out/announcement/l_administrative.json', 'r') as administrative:
         l_administrative = json.load(administrative)
     return l_administrative
 
@@ -107,7 +107,7 @@ def administrative():
 
 @app.route('/schedule', methods=["POST"])
 def schedule():
-    with open('./data/m_schedule.json', 'r') as schedule:
+    with open('./out/schedule/m_schedule.json', 'r') as schedule:
         m_schedule = json.load(schedule)
     return m_schedule
 
@@ -115,25 +115,25 @@ def schedule():
 
 @app.route('/anyang_to_school', methods=["POST"])
 def anyang_to_school():
-    with open('./data/m_anyang_to_school.json', 'r') as anyang_to_school:
+    with open('./out/schoolbus/m_anyang_to_school.json', 'r') as anyang_to_school:
         m_anyang_to_school = json.load(anyang_to_school)
     return m_anyang_to_school
 
 @app.route('/school_to_anyang', methods=["POST"])
 def school_to_anyang():
-    with open('./data/m_school_to_anyang.json', 'r') as school_to_anyang:
+    with open('./out/schoolbus/m_school_to_anyang.json', 'r') as school_to_anyang:
         m_school_to_anyang = json.load(school_to_anyang)
     return m_school_to_anyang
 
 @app.route('/beomgye_to_school', methods=["POST"])
 def beomgye_to_school():
-    with open('./data/m_beomgye_to_school.json', 'r') as beomgye_to_school:
+    with open('./out/schoolbus/m_beomgye_to_school.json', 'r') as beomgye_to_school:
         m_beomgye_to_school = json.load(beomgye_to_school)
     return m_beomgye_to_school
 
 @app.route('/school_to_beomgye', methods=["POST"])
 def school_to_beomgye():
-    with open('./data/m_school_to_beomgye.json', 'r') as school_to_beomgye:
+    with open('./out/schoolbus/m_school_to_beomgye.json', 'r') as school_to_beomgye:
         m_school_to_beomgye = json.load(school_to_beomgye)
     return m_school_to_beomgye
 

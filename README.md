bd1e87a5dfc250724171ef357f5ddb1a960cebad
hugesilver <xor3160@naver.com>
2023-10-16 01:47:53 +0900

    Changed from Flask to FastAPI

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 <img src="https://github.com/hugesilver/Daelimsik/blob/main/readme/Daelimsik_logo_Authority.jpg?raw=true" width="300px" height="300px"><br>
 
 ## 1️⃣ 소개
-2022년 여름방학에 개인적으로 진행한 첫 파이썬 프로젝트입니다.<br>
-방학기간 동안 나태해지지 말고 전공공부를 하자는 생각과 유용한 경험을 쌓기 위해,<br>
+2022년 여름방학에 개인적으로 진행한 첫 Python 프로젝트입니다.<br>
+방학 기간 동안 나태해지지 말고 전공 공부를 하자는 생각과 유용한 경험을 쌓기 위해,<br>
 "카카오톡 대림대학교 학식봇 대림식"을 기획하게 되었습니다.<br>
 
 \- 대림대학교 컴퓨터정보학부 19학번 학부생
@@ -29,8 +29,8 @@
     </td>
     </tr>
     </table>
-    <b>대림식</b>은 대림대학교 웹사이트 내 식단 페이지와 공지 페이지<br>
-    HTML 문서를 <u>BeautifulSoup</u>으로 혹은 <u>교내 서버</u>를 통해 <br>
+    <b>대림식</b>은 대림대학교 웹사이트 내 필요한 정보를<br>
+    <u>교내 API</u> 요청 혹은 <u>BeautifulSoup</u>으로 통해<br>
     데이터를 추출하여 카카오톡 스킬 형식에 맞게 JSON 파일로 변환합니다.
     <br><br>
 * 2️⃣ 서버
@@ -38,38 +38,32 @@
     <table>
     <tr>
     <td align="center">
-    <img src="https://github.com/hugesilver/Daelimsik/blob/main/readme/logo_aws.png?raw=true" width="100px" height="100px">
-    </td>
-    <td align="center">
     <img src="https://github.com/hugesilver/Daelimsik/blob/main/readme/logo_ubuntu.png?raw=true" width="100px" height="100px">
     </td>
     <td align="center">
-    <img src="https://github.com/hugesilver/Daelimsik/blob/main/readme/logo_flask.png?raw=true" width="100px" height="100px">
+    <img src="https://github.com/hugesilver/Daelimsik/blob/main/readme/logo_fastapi.png?raw=true" width="100px" height="100px">
     </td>
     </tr>
     <tr>
     <td align="center">
-    <b>AWS</b>
-    </td>
-    <td align="center">
     <b>Ubuntu</b>
     </td>
     <td align="center">
-    <b>Flask</b>
+    <b>FastAPI</b>
     </td>
     </tr>
     </table>
-    <b>대림식</b> 서버는 <u>Amazon Web Service EC2</u>를 이용하여 <u>Ubuntu</u> 운영체제를 사용하여<br>
-    작동되고 있습니다. 일정시간마다 Python 파일을 실행하게끔 명령하여 <u>Flask</u>로<br>
-    생성된 JSON 파일을 가지고 API 서버를 열어 카카오톡 서버와 통신합니다.
-    <br><br>
+    <b>대림식</b>은 <u>Ubuntu</u> 운영체제를 사용하여 작동되고 있습니다.<br>
+    일정 시간마다 Python 파일을 실행 명령을 하여 생성된 JSON 파일을 가지고<br>
+    <u>FastAPI</u>로 API 서버를 열어 카카오톡 서버와 통신합니다.<br>
+    <br>
 
 ## 3️⃣ 카카오톡 챗봇
-<img src="https://github.com/hugesilver/Daelimsik/blob/main/readme/image_kakao_business.png?raw=true" width="700px"><br><br>
-카카오톡 채널을 운영하기 위해서는 카카오비즈니스의 카카오 i 오픈빌더 서비스를 이용해야합니다.<br>
-단순 비즈니스 채널을 생성해 사용자와 1:1 채팅을 할 수 있는 서비스도 이용가능하지만,<br>
-챗봇 기능을 사용하여 상담원이 근무하는 시간 외에도 소통할 수 있다는 좋은 기능입니다.<br>
-이 기능을 이용하여 <b>대림식</b>에 24시간동안(서버가 문제 생기지 않는 이상 혹은 점검 중이 아닐 때)<br>
-정보 제공할 수 있는 프로젝트를 만들었습니다.<br>
-개인적으로 카카오톡 챗봇 구현에 시간을 많이 투자한 것 같습니다.<br>
+<img src="https://github.com/hugesilver/Daelimsik/blob/main/readme/image_kakao_business.png?raw=true" width="700px"><br>
+<br>
+기존 비즈니스 채널의 챗봇 기능을 활성화하기 위해<br>
+카카오비즈니스의 카카오 i 오픈빌더 서비스를 이용하여 챗봇 기능 사용 권한을 얻은 후,<br>
+챗봇 기능을 사용하여 24시간 동안 필요한 정보를 제공할 수 있는 프로젝트를 만들었습니다.<br>
+더욱 노력하는 <b>대림식</b>이 되겠습니다. 감사합니다.<br>
+<br>
 [[카카오 챗봇 가이드]](https://chatbot.kakao.com/docs/getting-started-overview)<br>
--- application.py
+++ application.py
@@ -1,54 +1,53 @@
-from flask import Flask
 import json
 
-from private_variables import port
+from fastapi import FastAPI
 
-app = Flask(__name__)
+app = FastAPI()
 
 
-@app.route('/')
+@app.get('/')
 def main():
-    return "<h2>Daelimsik 홈페이지</h2>"
+    return "대림식 연결 성공"
 
 
 # 학생식당
 
-@app.route('/student', methods=["POST"])
+@app.post('/student')
 def student():
     with open('./out/student/m_student_today.json', 'r') as student:
         m_student = json.load(student)
     return m_student
 
 
-@app.route('/student/mon', methods=["POST"])
+@app.post('/student/mon')
 def student_mon():
     with open('./out/student/m_student_mon.json', 'r') as student_mon:
         m_student_mon = json.load(student_mon)
     return m_student_mon
 
 
-@app.route('/student/tue', methods=["POST"])
+@app.post('/student/tue')
 def student_tue():
     with open('./out/student/m_student_tue.json', 'r') as student_tue:
         m_student_tue = json.load(student_tue)
     return m_student_tue
 
 
-@app.route('/student/wed', methods=["POST"])
+@app.post('/student/wed')
 def student_wed():
     with open('./out/student/m_student_wed.json', 'r') as student_wed:
         m_student_wed = json.load(student_wed)
     return m_student_wed
 
 
-@app.route('/student/thu', methods=["POST"])
+@app.post('/student/thu')
 def student_thu():
     with open('./out/student/m_student_thu.json', 'r') as student_thu:
         m_student_thu = json.load(student_thu)
     return m_student_thu
 
 
-@app.route('/student/fri', methods=["POST"])
+@app.post('/student/fri')
 def student_fri():
     with open('./out/student/m_student_fri.json', 'r') as student_fri:
         m_student_fri = json.load(student_fri)
@@ -57,42 +56,42 @@ def student_fri():
 
 # 교직원식당
 
-@app.route('/profstaff', methods=["POST"])
+@app.post('/profstaff')
 def profstaff():
     with open('./out/profstaff/m_profstaff_today.json', 'r') as profstaff:
         m_profstaff = json.load(profstaff)
     return m_profstaff
 
 
-@app.route('/profstaff/mon', methods=["POST"])
+@app.post('/profstaff/mon')
 def profstaff_mon():
     with open('./out/profstaff/m_profstaff_mon.json', 'r') as profstaff_mon:
         m_profstaff_mon = json.load(profstaff_mon)
     return m_profstaff_mon
 
 
-@app.route('/profstaff/tue', methods=["POST"])
+@app.post('/profstaff/tue')
 def profstaff_tue():
     with open('./out/profstaff/m_profstaff_tue.json', 'r') as profstaff_tue:
         m_profstaff_tue = json.load(profstaff_tue)
     return m_profstaff_tue
 
 
-@app.route('/profstaff/wed', methods=["POST"])
+@app.post('/profstaff/wed')
 def profstaff_wed():
     with open('./out/profstaff/m_profstaff_wed.json', 'r') as profstaff_wed:
         m_profstaff_wed = json.load(profstaff_wed)
     return m_profstaff_wed
 
 
-@app.route('/profstaff/thu', methods=["POST"])
+@app.post('/profstaff/thu')
 def profstaff_thu():
     with open('./out/profstaff/m_profstaff_thu.json', 'r') as profstaff_thu:
         m_profstaff_thu = json.load(profstaff_thu)
     return m_profstaff_thu
 
 
-@app.route('/profstaff/fri', methods=["POST"])
+@app.post('/profstaff/fri')
 def profstaff_fri():
     with open('./out/profstaff/m_profstaff_fri.json', 'r') as profstaff_fri:
         m_profstaff_fri = json.load(profstaff_fri)
@@ -101,21 +100,21 @@ def profstaff_fri():
 
 # 공지사항
 
-@app.route('/bachelor', methods=["POST"])
+@app.post('/bachelor')
 def bachelor():
     with open('./out/announcement/l_bachelor.json', 'r') as bachelor:
         l_bachelor = json.load(bachelor)
     return l_bachelor
 
 
-@app.route('/scholarship', methods=["POST"])
+@app.post('/scholarship')
 def scholarship():
     with open('./out/announcement/l_scholarship.json', 'r') as scholarship:
         l_scholarship = json.load(scholarship)
     return l_scholarship
 
 
-@app.route('/administrative', methods=["POST"])
+@app.post('/administrative')
 def administrative():
     with open('./out/announcement/l_administrative.json', 'r') as administrative:
         l_administrative = json.load(administrative)
@@ -124,7 +123,7 @@ def administrative():
 
 # 학사일정
 
-@app.route('/schedule', methods=["POST"])
+@app.post('/schedule')
 def schedule():
     with open('./out/schedule/m_schedule.json', 'r') as schedule:
         m_schedule = json.load(schedule)
@@ -133,47 +132,43 @@ def schedule():
 
 # 셔틀버스
 
-@app.route('/anyang_to_school', methods=["POST"])
+@app.post('/anyang_to_school')
 def anyang_to_school():
     with open('./out/schoolbus/m_anyang_to_school.json', 'r') as anyang_to_school:
         m_anyang_to_school = json.load(anyang_to_school)
     return m_anyang_to_school
 
 
-@app.route('/school_to_anyang', methods=["POST"])
+@app.post('/school_to_anyang')
 def school_to_anyang():
     with open('./out/schoolbus/m_school_to_anyang.json', 'r') as school_to_anyang:
         m_school_to_anyang = json.load(school_to_anyang)
     return m_school_to_anyang
 
 
-@app.route('/beomgye_to_school', methods=["POST"])
+@app.post('/beomgye_to_school')
 def beomgye_to_school():
     with open('./out/schoolbus/m_beomgye_to_school.json', 'r') as beomgye_to_school:
         m_beomgye_to_school = json.load(beomgye_to_school)
     return m_beomgye_to_school
 
 
-@app.route('/school_to_beomgye', methods=["POST"])
+@app.post('/school_to_beomgye')
 def school_to_beomgye():
     with open('./out/schoolbus/m_school_to_beomgye.json', 'r') as school_to_beomgye:
         m_school_to_beomgye = json.load(school_to_beomgye)
     return m_school_to_beomgye
 
 
-@app.route('/help_anyang', methods=["POST"])
+@app.post('/help_anyang')
 def help_anyang():
     with open('./out/schoolbus/m_help_anyang.json', 'r') as help_anyang:
         m_help_anyang = json.load(help_anyang)
     return m_help_anyang
 
 
-@app.route('/help_beomgye', methods=["POST"])
+@app.post('/help_beomgye')
 def help_beomgye():
     with open('./out/schoolbus/m_help_beomgye.json', 'r') as help_beomgye:
         m_help_beomgye = json.load(help_beomgye)
     return m_help_beomgye
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=port, debug=True)
